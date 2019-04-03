268dbecaf9539a201b7fe64269ed62fe9d6f0b2a
Kidsan <8798449+Kidsan@users.noreply.github.com>
2019-04-03 20:40:06 +0100

    Create book related structs

---
+++ book/book.go
@@ -0,0 +1,13 @@
+package book
+
+type Author struct {
+	Firstname string `json:firstname`
+	Lastname string `json:lastname`
+}
+
+type Book struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Author *Author `json:"author"`
+}
