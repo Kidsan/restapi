944501dbd52a8ade4e9c98e946ba78053758fb76
Kidsan <8798449+Kidsan@users.noreply.github.com>
2019-04-04 21:46:41 +0100

    Start rewriting calls to use echo instead of mux

--- main.go
+++ main.go
@@ -1,68 +1,65 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
-	"math/rand"
-	"strconv"
-	book"restapi/book"
-
-	"github.com/gorilla/mux"
+	"restapi/book"
 )
 
 //books
 var books []book.Book
 
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type","application/json")
-	json.NewEncoder(w).Encode(books)
+func getBooks(c echo.Context) error {
+	return c.JSON(http.StatusOK, books)
 }
 
-func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type","application/json")
-	params := mux.Vars(r)
+func getBook(c echo.Context) error {
+	id := c.Param("id")
 
 	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
+		if item.ID == id {
+			return c.JSON(http.StatusOK, item)
 		}
 	}
-	json.NewEncoder(w).Encode(&book.Book{})
+
+	return c.JSON(http.StatusNotFound,"Not Found")
 }
+//
+//func createBook(c echo.Context) error) {
+//	w.Header().Set("Content-type", "application/json")
+//	var book book.Book
+//	_ = json.NewDecoder(r.Body).Decode(&book)
+//	book.ID = strconv.Itoa(rand.Intn(10000000)) //not safe obviously
+//	books = append(books, book)
+//
+//	json.NewEncoder(w).Encode(book)
+//}
 
-func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type","application/json")
-	var book book.Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(10000000)) //not safe obviously
-	books = append(books, book)
-
-	json.NewEncoder(w).Encode(book)
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-
-}
+//func updateBook(w http.ResponseWriter, r *http.Request) {
+//
+//}
+//
+//func deleteBook(w http.ResponseWriter, r *http.Request) {
+//
+//}
 
 func main() {
 	//init router
 
-	router := mux.NewRouter()
+	e := echo.New()
 
-	books = append(books, book.Book{ID: "1", Isbn: "543543", Title: "Book One", Author: &book.Author{ Firstname: "John", Lastname: "Doe"}})
-	books = append(books, book.Book{ID: "2", Isbn: "12345", Title: "Book Two", Author: &book.Author{ Firstname: "Jack", Lastname: "Don't"}})
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	router.HandleFunc("/api/books", getBooks).Methods("GET")
-	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/api/books",createBook).Methods("POST")
-	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	books = append(books, book.Book{ID: "1", Isbn: "543543", Title: "Book One", Author: &book.Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, book.Book{ID: "2", Isbn: "12345", Title: "Book Two", Author: &book.Author{Firstname: "Jack", Lastname: "Don't"}})
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	e.GET("/api/books", getBooks)
+	e.GET("/api/books/:id", getBook)
+	//e.POST("/api/books", createBook)
+	//router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	//router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	e.Logger.Fatal(e.Start(":8000"))
 }
